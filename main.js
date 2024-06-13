a9dce4ad1257b8215aa09635a1bd44280f0b3e53
guillermogm <guillermogm009@gmail.com>
2024-06-13 20:33:56 +0200

    feat: First version of empanadas, gives some errors

--- main.js
+++ main.js
@@ -10,25 +10,68 @@ export const fn = (a, b, c) => {
    if(a + b + c > 40) {
       throwError("No puedes poner mas de un total de 40 empanadas")
    }
-
+   
+   let preciofinal = 0;
    let empanadasMitadCarasBaratas = 0;
    let empanadasMitadCarasIntermedias = 0;
    let empanadasMitadIntermediasBaratas = 0;
-
-  // Reducción de empanadas hasta hacer las "medias-empanadas" máximas posibles
-  while (a > 0 && c > 0) {
-   a--;
-   c--;
-   empanadasMitadCarasBaratas += 2;
+   let totalEmpanadas = a + b + c;
+   let iteraciones = Math.floor(totalEmpanadas / 3);
+ 
+   // Reducción de empanadas hasta hacer las "medias-empanadas" máximas posibles
+   while (a > 0 && c > 0) {
+     a--;
+     c--;
+     empanadasMitadCarasBaratas += 2;
    }
    while (b > 0 && c > 0) {
-      b--;
-      c--;
-      empanadasMitadCarasIntermedias += 2;
-    }
-    while (a > 0 && b > 0) {
-      a--;
-      b--;
-      empanadasMitadIntermediasBaratas += 2;
-    }
-};
+     b--;
+     c--;
+     empanadasMitadCarasIntermedias += 2;
+   }
+   while (a > 0 && b > 0) {
+     a--;
+     b--;
+     empanadasMitadIntermediasBaratas += 2;
+   }
+ 
+   // Formación de grupos de 3 empanadas cobrándose la de más valor
+   for (let i = 0; i < iteraciones; i++) {
+     let grupo = [];
+     let grupoPrecio = 0;
+ 
+     // Seleccionar empanadas para el grupo
+     if (c > 0) {
+       grupo.push({ precio: 16 });
+       c--;
+     }
+     if (empanadasMitadCarasIntermedias > 0) {
+       grupo.push({ precio: 15 });
+       empanadasMitadCarasIntermedias--;
+     }
+     if (b > 0) {
+       grupo.push({ precio: 14 });
+       b--;
+     }
+     if (empanadasMitadCarasBaratas > 0) {
+       grupo.push({ precio: 14 });
+       empanadasMitadCarasBaratas--;
+     }
+     if (empanadasMitadIntermediasBaratas > 0) {
+       grupo.push({ precio: 13 });
+       empanadasMitadIntermediasBaratas--;
+     }
+     if (a > 0) {
+       grupo.push({ precio: 12 });
+       a--;
+     }
+ 
+     // Calcular precio del grupo
+     grupoPrecio = Math.max(...grupo.map(empanada => empanada.precio));
+ 
+     // Agregar precio del grupo al total
+     preciofinal += grupoPrecio;
+   }
+ 
+   return preciofinal;
+ };
